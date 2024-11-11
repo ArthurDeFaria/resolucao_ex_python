a4ecd68bfa17459b7122e3a5bda8d1036fbcf0b2
ArthurDeFaria <128834352+ArthurDeFaria@users.noreply.github.com>
2024-11-11 15:46:17 -0300

    Add files via upload

---
+++ resolucao_ex_aula01.py
@@ -0,0 +1,18 @@
+# Calculadora de soma
+
+numero1 = input("Digite o primeiro numero: ")
+numero2 = input("Digite o segundo numero: ")
+soma = int(numero1) + int(numero2)
+print("A soma é", soma)
+
+# Conversor de unidades
+
+kms = input("Digite a distancia em km: ")
+cms = float(kms)*100000.0
+print("A distancia é ", cms , "em cm")
+
+# Calculo de área do círculo
+
+raio = float(input("Digite o valor da raio: "))
+areaCirculo = 3.14*raio*raio
+print("A área do círculo é aproximadamente ", areaCirculo)
